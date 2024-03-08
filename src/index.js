628127d9b5cbe4133367c3f082d79cfad0d954eb
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-03-08 20:54:40 +0530

    cloudinary setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import connectDb from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDb();
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server started at port 8000");
+    });
+  })
+  .catch( (error) => {
+    console.log("MongoDb connection failed: ",error);
+  });
 
 /*
 // problem
